f89c7e2bf03bc30793277709d2bac32a143a246b
PH <leopodinoraphael@gmail.com>
2026-04-02 22:10:59 -0300

    feat: Creating skeleton and adding navegation bar

--- app/src/main/java/com/example/nexoapp/network/ApiService.kt
+++ app/src/main/java/com/example/nexoapp/network/ApiService.kt
@@ -1,15 +1,34 @@
-#!/usr/bin/env kotlin
 package com.example.nexoapp.network
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-// Modelo de dados para o login
+// --- AS SUAS DATA CLASSES (Os moldes dos dados) ---
+
 data class LoginRequest(val email: String, val senha: String)
+
 data class LoginResponse(val token: String)
 
+// Esta é a nova que criamos para o cadastro!
+data class RegisterRequest(
+    val name: String,
+    val email: String,
+    val password: String,
+    val alias: String
+)
+
+// --- A SUA ÚNICA INTERFACE DA API ---
+// É aqui que ficam todas as rotas do seu backend
+
 interface ApiService {
-    @POST("auth/login") // O endpoint que o Lucas está criando
+
+    // Rota antiga de Login (que você já tinha)
+    @POST("auth/login")
     fun login(@Body request: LoginRequest): Call<LoginResponse>
+
+    // Nova Rota de Cadastro que aponta pro seu Controller do Spring Boot
+    @POST("auth/register")
+    suspend fun registerUser(@Body request: RegisterRequest): Response<Void>
 }
